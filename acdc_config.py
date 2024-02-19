e67eab877a69b48c9c569346ab708a00c9344df7
Marc-Antoine Lavoie <lavoie.marcantoine4012@gmail.com>
2024-02-19 17:27:48 -0500

    add specific config

--- acdc_config.py
+++ acdc_config.py
@@ -3,16 +3,23 @@ import yaml
 
 # dataset = 'rain'
 dataset = sys.argv[1]
+if len(sys.argv) > 2:
+    run_type = sys.argv[2]
+else:
+    run_type = 'faster_rcnn_VGG_cross_city_prob.yaml'
+
+config_file = "adaptive_teacher/configs/" + run_type
+output_file = config_file.rsplit('/',1)[0] + "/faster_rcnn_VGG_cross_city_acdc.yaml"
 
 file_train = '("ACDC_train_{}",)'.format(dataset)
 file_test = '("ACDC_val_{}",)'.format(dataset)
 
-with open("adaptive_teacher/configs/faster_rcnn_VGG_cross_city_prob.yaml") as f:
+with open(config_file) as f:
     y = yaml.safe_load(f)
     y['DATASETS']['TRAIN_UNLABEL'] = file_train
     y['DATASETS']['TEST'] = file_test
     print
 
-with open("adaptive_teacher/configs/faster_rcnn_VGG_cross_city_acdc.yaml", 'w') as f:
+with open(output_file, 'w') as f:
     yaml.dump(y, f, default_flow_style=False, sort_keys=False)
 
